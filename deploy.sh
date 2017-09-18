ac70e2a5a4a456f7d0d561f1592e38e7940a583b
Matthias Kretschmann <m@kretschmann.io>
2017-09-18 23:54:01 +0200

    deployment script rewrite

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,30 @@
 #!/usr/bin/env bash
 
-npm run build && \
-aws s3 sync ./build s3://oceanprotocol.com --delete --acl public-read
+set -e;
+
+##
+## check for pull request against master
+##
+if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+
+    aws s3 sync ./build s3://beta.oceanprotocol.com --delete --acl public-read
+
+
+##
+## check for master push which is no pull request
+##
+elif [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+
+    aws s3 sync ./build s3://oceanprotocol.com --delete --acl public-read
+
+else
+    echo "---------------------------------------------"
+    echo "      ✓ nothing to deploy "
+    echo "---------------------------------------------"
+fi;
+
+echo "---------------------------------------------"
+echo "         ✓ done deployment "
+echo "---------------------------------------------"
+
+exit;
