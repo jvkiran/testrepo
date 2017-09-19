79c2bea99d6bf4010928b8472aba74a67dac00e8
Matthias Kretschmann <m@kretschmann.io>
2017-09-19 19:55:07 +0200

    move icons into component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
+import SubscribeForm from 'react-mailchimp-subscribe';
 import oceanLogo from './assets/ocean-logo.svg';
 import bigchainDBLogo from './assets/bigchain-db.svg';
 import dexLogo from './assets/dex.svg';
 import { colors, layout, responsive } from './styles';
-import SubscribeForm from 'react-mailchimp-subscribe';
+import { IconTwitter, IconFacebook } from './icons';
 
 const formProps = {
   action: '//oceanprotocol.us16.list-manage.com/subscribe/post?u=cd10df7575858374f6a066d13&amp;id=3c6eed8b71',
@@ -246,21 +247,17 @@ const App = () => (
       <StyledLinks>
           <div>
               <h2 className="dimmed">Announcement</h2>
-              <a href="https://medium.com/oceanprotocol/from-ai-to-blockchain-to-data-meet-ocean-f210ff460465">From AI to Blockchain to Data: Meet Ocean</a>
+              <a href="https://medium.com/oceanprotocol/from-ai-to-blockchain-to-data-meet-ocean-f210ff460465">
+                From AI to Blockchain to Data: Meet Ocean
+              </a>
           </div>
           <div>
               <h2 className="dimmed">Follow</h2>
               <a className="social-link" href="https://twitter.com/oceanprotocol" title="Twitter">
-                  <svg className="icon icon--twitter" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
-                    <path d="M17.316,4.246 C17.324,4.408 17.327,4.572 17.327,4.734 C17.327,9.724 13.53,15.476 6.587,15.476 C4.454,15.476 2.471,14.851 0.8,13.779 C1.096,13.814 1.396,13.832 1.7,13.832 C3.47,13.832 5.097,13.228 6.388,12.217 C4.737,12.186 3.342,11.096 2.862,9.596 C3.092,9.639 3.329,9.662 3.572,9.662 C3.917,9.662 4.251,9.617 4.567,9.531 C2.84,9.183 1.539,7.658 1.539,5.828 C1.539,5.812 1.539,5.797 1.539,5.781 C2.048,6.064 2.631,6.234 3.249,6.254 C2.236,5.576 1.569,4.422 1.569,3.111 C1.569,2.42 1.755,1.771 2.081,1.213 C3.942,3.498 6.725,5 9.862,5.158 C9.798,4.881 9.765,4.594 9.765,4.297 C9.765,2.213 11.454,0.524 13.539,0.524 C14.625,0.524 15.606,0.981 16.295,1.715 C17.154,1.545 17.962,1.231 18.692,0.799 C18.41,1.68 17.811,2.42 17.032,2.887 C17.796,2.795 18.522,2.594 19.2,2.293 C18.694,3.051 18.054,3.715 17.316,4.246 Z" transform="translate(0 2)"/>
-                  </svg>
+                  <IconTwitter />
               </a>
-              
               <a className="social-link" href="https://facebook.com/oceanprotocol" title="Facebook">
-              <svg className="icon icon--facebook" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
-                <path d="M16,0 L2,0 C0.9,0 0,0.9 0,2 L0,16 C0,17.101 0.9,18 2,18 L9,18 L9,11 L7,11 L7,8.525 L9,8.525 L9,6.475 C9,4.311 10.212,2.791 12.766,2.791 L14.569,2.793 L14.569,5.398 L13.372,5.398 C12.378,5.398 12,6.144 12,6.836 L12,8.526 L14.568,8.526 L14,11 L12,11 L12,18 L16,18 C17.1,18 18,17.101 18,16 L18,2 C18,0.9 17.1,0 16,0 Z" transform="translate(1 1)"/>
-              </svg>
-
+                <IconFacebook />
               </a>
           </div>
       </StyledLinks>
