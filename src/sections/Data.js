db25069b1a06a9055974a40089792c9adae321fe
Pedro Gomes <pedrogomes94@gmail.com>
2017-09-29 21:45:09 +0200

    adjust data section mobile

--- src/sections/Data.js
+++ src/sections/Data.js
@@ -12,53 +12,72 @@ import dataLocked from '../assets/graphics/data-locked.svg';
 import dataUnavailable from '../assets/graphics/data-unavailable.svg';
 import { responsive } from '../styles';
 
-const StyledGrid = styled(Grid)`@media screen and (${responsive.sm.max}) {margin-top: 4rem;}`;
-
-const StyledFirstGrid = styled(StyledGrid)`align-items: flex-end;`;
-
-const StyledSecondGrid = styled(StyledGrid)`
-  align-items: center;
-  @media screen and (${responsive.sm.min}) {
-    margin: 4rem 0;
-  }
-`;
+const StyledGrid = styled(Grid)`@media screen and (${responsive.sm.max}) {margin-top: 6rem;}`;
 
 const StyledUnalyzed = styled.img`
   width: auto;
   height: 100%;
+  max-height: 300px;
   @media screen and (${responsive.sm.min}) {
+    max-width: 300px;
     padding: 0;
     margin: 0;
     width: 100%;
     height: 100%;
-    max-width: 300px;
-    max-height: 300px;
   }
   @media screen and (${responsive.sm.max}) {
     padding: 1rem 3rem;
   }
 `;
 
+const StyledFirstGrid = styled(StyledGrid)`
+  align-items: flex-end;
+  @media screen and (${responsive.sm.max}) {
+    text-align: center;
+  }
+`;
+
 const StyledLocked = styled.img`
   padding: 3rem 6rem 0 0;
+  max-height: 300px;
   @media screen and (${responsive.sm.min}) {
+    max-width: 300px;
     padding: 0;
     margin: 0;
     width: 100%;
     height: 100%;
-    max-width: 300px;
-    max-height: 300px;
   }
   @media screen and (${responsive.sm.max}) {
     padding: 0 2rem;
   }
 `;
 
+const StyledSecondGrid = styled(StyledGrid)`
+  align-items: center;
+  @media screen and (${responsive.sm.min}) {
+    margin: 4rem 0;
+  }
+  @media screen and (${responsive.sm.max}) {
+    text-align: center;
+  }
+`;
+
 const StyledUnavailable = styled.img`
   margin: 3rem 0;
+  max-height: 300px;
   @media screen and (${responsive.sm.max}) {
     width: 300% !important;
+    margin: 0;
     margin-left: -100%;
+    margin-bottom: 1rem;
+  }
+`;
+
+const StyledThirdGrid = styled(StyledGrid)`
+  @media screen and (${responsive.sm.max}) {
+    & > div:nth-child(2) {
+      order: -1;
+    }
   }
 `;
 
@@ -93,7 +112,7 @@ const Data = () => (
         </Cell>
       </StyledSecondGrid>
 
-      <StyledGrid wrap textCenter>
+      <StyledThirdGrid wrap textCenter>
         <Cell width={1}>
           <SubTitle>Data is widely available but owned by few</SubTitle>
         </Cell>
@@ -106,7 +125,7 @@ const Data = () => (
             and most startups are starving for data.
           </Paragraph>
         </Cell>
-      </StyledGrid>
+      </StyledThirdGrid>
     </ContentRow>
   </Section>
 );
