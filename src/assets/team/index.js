9c33dc1570fe280eead28065fede51fe146a3a5d
Matthias Kretschmann <m@kretschmann.io>
2017-10-04 13:58:38 +0200

    more team photos

--- src/assets/team/index.js
+++ src/assets/team/index.js
@@ -5,15 +5,20 @@ import daniel from './daniel.jpg';
 import daryl from './daryl.jpg';
 import dimi from './dimi.jpg';
 import dinh from './dinh.jpg';
+import don from './don.jpg';
 import gautam from './gautam.jpg';
 import greg from './greg.jpg';
 import jernej from './jernej.jpg';
 import kamal from './kamal.jpg';
 import kasia from './kasia.jpg';
 import masha from './masha.jpg';
+import morgan from './morgan.jpg';
 import muawia from './muawia.jpg';
+import oana from './oana.jpg';
 import rodolphe from './rodolphe.jpg';
 import sarah from './sarah.jpg';
+import shahbaz from './shahbaz.jpg';
+import sylvain from './sylvain.jpg';
 import timothy from './timothy.jpg';
 import trent from './trent.jpg';
 import troy from './troy.jpg';
@@ -27,15 +32,20 @@ export default {
   daryl,
   dimi,
   dinh,
+  don,
   gautam,
   greg,
   jernej,
   kamal,
   kasia,
   masha,
+  morgan,
   muawia,
+  oana,
   rodolphe,
   sarah,
+  shahbaz,
+  sylvain,
   timothy,
   trent,
   troy,
