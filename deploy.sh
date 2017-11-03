5e17fb0245148e5bbebc1d2ae0d05c3c092c8e94
Matthias Kretschmann <m@kretschmann.io>
2017-11-03 12:56:25 +0100

    activate beta deployment

--- deploy.sh
+++ deploy.sh
@@ -5,15 +5,15 @@ set -e;
 ##
 ## check for pull request against master
 ##
-#if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
 
-    #aws s3 sync ./build s3://beta.oceanprotocol.com --delete --acl public-read
+    aws s3 sync ./build s3://beta.oceanprotocol.com --delete --acl public-read
 
 
 ##
 ## check for master push which is no pull request
 ##
-if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+elif [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 
     aws s3 sync ./build s3://oceanprotocol.com --delete --acl public-read
 
