6012d12b82b7c574f83b2693cfbfcc8db297a209
Matthias Kretschmann <m@kretschmann.io>
2017-11-30 18:01:01 +0100

    add service worker

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import { render } from 'react-snapshot';
 import { injectGlobal } from 'styled-components';
 import { globalStyles } from './styles';
 import Root from './Root';
+import registerServiceWorker from './registerServiceWorker';
 
 // eslint-disable-next-line
 injectGlobal`${globalStyles}`;
 
 render(<Root />, document.getElementById('root'));
+registerServiceWorker();
