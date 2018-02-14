6d6506cd56460855fda327979d50f15cc7dbd7ec
Matthias Kretschmann <m@kretschmann.io>
2018-02-14 11:31:57 +0100

    the 9am button

--- src/Root.js
+++ src/Root.js
@@ -5,6 +5,7 @@ import Menu from './sections/Menu'
 import Welcome from './sections/Welcome'
 import Project from './sections/Project'
 import Data from './sections/Data'
+import DataVideo from './sections/DataVideo'
 import Papers from './sections/Papers'
 import Events from './sections/Events'
 import Team from './sections/Team'
@@ -43,6 +44,7 @@ class Root extends Component {
           <Welcome />
           <Events />
           <Project toggleModal={this.toggleModal} />
+          <DataVideo />
           <Papers />
           <Data />
           <Team toggleModal={this.toggleModal} />
