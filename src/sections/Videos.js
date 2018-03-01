837a285b4015035ba3ee4b021795776e6b9b2acd
Matthias Kretschmann <m@kretschmann.io>
2018-03-01 11:49:07 +0100

    video player styling tweaks

--- src/sections/Videos.js
+++ src/sections/Videos.js
@@ -10,10 +10,10 @@ import Section from '../components/Section'
 import Title from '../components/Title'
 import Paragraph from '../components/Paragraph'
 import ContentRow from '../components/ContentRow'
-import videos from '../data/videos.json'
+import videos from '../data/videos'
 import playIcon from '../assets/misc/play-circle.svg'
 import cross from '../assets/misc/cross.svg'
-import { colors, fonts } from '../styles'
+import { colors, fonts, transitions } from '../styles'
 
 const Background = styled.div`
     position: absolute;
@@ -32,7 +32,7 @@ const StyledTitle = styled(Title)`
 `
 
 const StyledContentRow = styled(ContentRow)`
-    max-width: 74rem;
+    max-width: 60rem;
     z-index: 1;
 
     .slick-dots {
@@ -96,7 +96,7 @@ const PlayButton = styled.img`
 const VideoContainer = styled.div`
     position: relative;
     margin: 10px;
-    border: .4rem solid rgb(${colors.black});
+    border: .4rem solid rgb(${colors.white});
     background: rgb(${colors.black});
 `
 
@@ -109,7 +109,8 @@ const StyledClose = styled.img`
     right: 1.5rem;
     z-index: 21;
 
-    &:hover {
+    &:hover,
+    &:focus {
         opacity: .7;
     }
 `
@@ -139,25 +140,47 @@ const VideoListItem = styled.div`
 `
 
 const ListContainer = styled.div`
-    margin: 0 10px;
-    color: rgb(${colors.black});
-    background: rgb(${colors.white});
+    margin: 0 .5rem;
+    color: rgb(${colors.lightGrey});
+    background: rgb(${colors.black});
+    border-radius: 2px;
+    box-shadow: 0 9px 18px 0 rgba(${colors.black}, .3);
+    transition: ${transitions.short};
+    border: .08rem solid rgb(${colors.grey});
+
+    &:hover,
+    &:focus {
+        box-shadow: 0 12px 30px 0 rgba(${colors.black}, .3);
+        transform: translate3d(0, -.05rem, 0);
+    }
+
+    &:active {
+        box-shadow: 0 9px 18px 0 rgba(${colors.black}, .3);
+        transition: none;
+        transform: none;
+    }
+
+    &.active {
+        background: rgb(${colors.white});
+        color: rgb(${colors.black});
+    }
 `
 
 const VideoThumb = styled.img`
     padding: .4rem;
     background: rgb(${colors.black});
 
-    &.active {
+    .active & {
         background: rgb(${colors.white});
     }
 `
 
 const ThumbTitle = styled(Paragraph)`
-    padding: 1.25rem;
+    padding: .5rem 1.25rem 1rem 1.25rem;
     margin: 0;
     text-align: center;
     font-family: ${fonts.family.button};
+    font-size: ${fonts.size.small};
 
     span {
         overflow: hidden;
@@ -230,8 +253,8 @@ class SectionContent extends Component {
                 <Slider {...settings}>
                     {this.props.items.map((properties, index) => (
                         <VideoListItem key={index} onClick={() => this.selectVideo(properties, index)}> {/* eslint-disable-line react/no-array-index-key*/}
-                            <ListContainer>
-                                <VideoThumb alt="video thumbnail" className={this.state.active === index ? 'active' : ''} src={properties.snippet.thumbnails.medium.url} />
+                            <ListContainer className={this.state.active === index ? 'active' : ''}>
+                                <VideoThumb alt="video thumbnail" src={properties.snippet.thumbnails.medium.url} />
                                 <ThumbTitle><span>{properties.snippet.title}</span></ThumbTitle>
                             </ListContainer>
                         </VideoListItem>
