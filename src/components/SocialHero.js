23563e7198ff81e09674d8ce29330efc045833fe
Matthias Kretschmann <m@kretschmann.io>
2018-03-05 14:38:33 +0100

    remove Telegram group link

--- src/components/SocialHero.js
+++ src/components/SocialHero.js
@@ -39,7 +39,6 @@ const SocialBand = styled.aside`
 const SocialHero = () => (
     <SocialBand>
         <a href={social.telegram_news} rel="noopener noreferrer" target="_blank"><img alt="Telegram" src={buttonTelegram} />Telegram News Channel</a>
-        <a href={social.telegram} rel="noopener noreferrer" target="_blank"><img alt="Telegram" src={buttonTelegram} />Telegram Group Chat</a>
         <a href={social.twitter} rel="noopener noreferrer" target="_blank"><img alt="Twitter" src={buttonTwitter} />Twitter</a>
     </SocialBand>
 )
