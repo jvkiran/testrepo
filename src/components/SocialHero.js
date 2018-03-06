8a5c2a3eef6272fa7e9c0ceb9baafa4d50aef80d
Matthias Kretschmann <m@kretschmann.io>
2018-03-06 16:52:41 +0100

    add blog link to social hero

--- src/components/SocialHero.js
+++ src/components/SocialHero.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 import { responsive, colors, fonts } from '../styles'
 import buttonTelegram from '../assets/buttons/telegram.svg'
 import buttonTwitter from '../assets/buttons/twitter.svg'
+import buttonMedium from '../assets/buttons/medium.svg'
 import { social } from '../constants'
 
 const SocialBand = styled.aside`
@@ -40,6 +41,7 @@ const SocialHero = () => (
     <SocialBand>
         <a href={social.telegram_news} rel="noopener noreferrer" target="_blank"><img alt="Telegram" src={buttonTelegram} />Telegram News Channel</a>
         <a href={social.twitter} rel="noopener noreferrer" target="_blank"><img alt="Twitter" src={buttonTwitter} />Twitter</a>
+        <a href={social.blog} rel="noopener noreferrer" target="_blank"><img alt="Blog" src={buttonMedium} />Blog</a>
     </SocialBand>
 )
 
