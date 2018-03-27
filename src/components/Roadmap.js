2dab78e7775ebd9c9fad6f4d8b7f7f7172641902
Matthias Kretschmann <m@kretschmann.io>
2018-03-27 17:36:20 +0200

    styling tweaks, modified milestones

--- src/components/Roadmap.js
+++ src/components/Roadmap.js
@@ -19,7 +19,7 @@ const FlexContentRow = styled(ContentRow)`
     padding-top: 2rem;
     padding-left: 2rem;
 
-    @media screen and (${responsive.md.min}) {
+    @media screen and (${responsive.lg.min}) {
         display: flex;
         padding-top: 6rem;
         padding-left: 0;
@@ -27,21 +27,22 @@ const FlexContentRow = styled(ContentRow)`
     }
 `
 
-const MilestoneTitle = styled.h5`
+const MilestoneTitle = styled.h1`
+    font-size: ${fonts.size.base};
     margin: 0;
     margin-bottom: .25rem;
 
-    @media screen and (${responsive.md.min}) {
+    @media screen and (${responsive.lg.min}) {
         margin-top: -5rem;
     }
 `
 
-const MilestoneDate = styled.h5`
+const MilestoneDate = styled.h2`
+    font-size: ${fonts.size.base};
     color: rgb(${colors.lightGrey});
     margin: 0;
-    font-size: ${fonts.size.base};
 
-    @media screen and (${responsive.md.min}) {
+    @media screen and (${responsive.lg.min}) {
         margin-bottom: 4rem;
     }
 `
@@ -50,10 +51,13 @@ const MilestoneDescription = styled.p`
     margin-top: 1rem;
     margin-bottom: 0;
 
-    @media screen and (${responsive.md.min}) {
-        padding-left: 1rem;
-        padding-right: 1rem;
+    @media screen and (${responsive.lg.min}) {
+        padding-left: .75rem;
+        padding-right: .75rem;
         font-size: ${fonts.size.small};
+        max-width: 15rem;
+        margin-left: auto;
+        margin-right: auto;
     }
 `
 
@@ -65,9 +69,10 @@ const MilestoneBullet = styled.div`
     background: rgb(${colors.white});
     position: absolute;
     left: -1.05rem;
-    top: 0;
+    top: -.25rem;
+    z-index: 2;
 
-    @media screen and (${responsive.md.min}) {
+    @media screen and (${responsive.lg.min}) {
         top: -1.05rem;
         left: 50%;
         margin-left: -1rem;
@@ -88,20 +93,38 @@ const MilestoneBullet = styled.div`
 const Milestone = styled.div`
     padding-bottom: 3rem;
     position: relative;
-    padding-left: 3rem;
+    padding-left: 2rem;
+    padding-right: 2rem;
     margin-left: 1rem;
     border-left: .15rem solid;
     border-left-color: ${({ active }) => (active ? `rgb(${colors.pink})` : `rgb(${colors.lightGrey})`)};
     flex: 1;
 
-    @media screen and (${responsive.md.min}) {
+    @media screen and (${responsive.lg.min}) {
         border-left: 0;
         border-top: .15rem solid;
         border-top-color: ${({ active }) => (active ? `rgb(${colors.pink})` : `rgb(${colors.lightGrey})`)};
         padding-left: 0;
+        padding-right: 0;
         padding-bottom: 0;
         margin-left: 0;
         text-align: center;
+
+        &:first-child {
+            padding-left: 2rem;
+
+            ${MilestoneBullet} { /* stylelint-disable-line */
+                margin-left: 0;
+            }
+        }
+
+        &:last-child {
+            padding-right: 2rem;
+
+            ${MilestoneBullet} { /* stylelint-disable-line */
+                margin-left: -2rem;
+            }
+        }
     }
 
     ${MilestoneTitle}, ${MilestoneDate}, ${MilestoneDescription} { /* stylelint-disable-line */
@@ -115,7 +138,29 @@ const Milestone = styled.div`
             background: ${({ active }) => (active ? `rgb(${colors.pink})` : `rgb(${colors.dimmedGrey})`)};
         }
     }
+
+    @media screen and (${responsive.lg.min}) {
+        &:nth-last-child(1 of .active):after { /* selector only works in Safari at the moment */
+            content: '';
+            width: 50%;
+            height: .125rem;
+            display: block;
+            background: rgb(${colors.lightGrey});
+            position: absolute;
+            left: 50%;
+            top: -.125rem;
+        }
+    }
 `
+
+const Milestones = roadmap.map((milestone) => (
+    <Milestone active={milestone.active} className={!!milestone.active && 'active'} key={milestone.title}>
+        <MilestoneTitle>{milestone.title}</MilestoneTitle>
+        <MilestoneDate>{milestone.date}</MilestoneDate>
+        <MilestoneDescription>{milestone.description}</MilestoneDescription>
+        <MilestoneBullet />
+    </Milestone>
+))
 
 const Roadmap = () => (
     <StyledRoadmap>
@@ -123,14 +168,7 @@ const Roadmap = () => (
             <Title>Roadmap</Title>
         </ContentRow>
         <FlexContentRow wide>
-            {roadmap.map((milestone) => (
-                <Milestone active={milestone.active} key={milestone.title}>
-                    <MilestoneTitle>{milestone.title}</MilestoneTitle>
-                    <MilestoneDate>{milestone.date}</MilestoneDate>
-                    <MilestoneDescription>{milestone.description}</MilestoneDescription>
-                    <MilestoneBullet />
-                </Milestone>
-            ))}
+            {Milestones}
         </FlexContentRow>
     </StyledRoadmap>
 )
