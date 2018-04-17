eabc617e8e65829beda0972f0364011ec0098dae
Matthias Kretschmann <m@kretschmann.io>
2018-04-17 14:36:53 +0200

    auto hex color conversion, add logo, add mediakit download

--- src/pages/Branding.js
+++ src/pages/Branding.js
@@ -1,14 +1,19 @@
 import React, { Fragment } from 'react'
 import Helmet from 'react-helmet'
 import styled from 'styled-components'
+import rgbHex from 'rgb-hex'
 import Header from '../sections/Header'
+import Button from '../components/Button'
 import Section from '../components/Section'
 import Title from '../components/Title'
 import SubTitle from '../components/SubTitle'
 import ContentRow from '../components/ContentRow'
-import { colors, gradients, responsive } from '../styles'
+import { colors, fonts, gradients, responsive } from '../styles'
+import OceanLogo from '../assets/logos/ocean-logo.svg'
+import OceanLogoDark from '../assets/logos/ocean-logo-dark.svg'
 
 const pageTitle = 'Branding'
+const mediakitDownload = 'https://github.com/oceanprotocol/art/archive/master.zip'
 
 const Colors = styled.div`
     display: flex;
@@ -30,16 +35,23 @@ const Color = styled.div`
         flex-basis: 32%;
     }
 
+    span { display: block; }
+
     .color__name {
-        display: block;
+
     }
 
     .color__value {
     }
 `
 
+const Gradients = styled(Colors)`
+
+`
+
 const Gradient = styled(Color)`
     background: ${props => (props.color ? props.color : '')};
+    flex-basis: 100%;
 `
 
 const Meta = () => (
@@ -59,6 +71,16 @@ const Branding = () => (
             </ContentRow>
 
             <ContentRow>
+                <Button>
+                    <a download href={mediakitDownload}>Download Mediakit</a>
+                </Button>
+            </ContentRow>
+
+            <ContentRow>
+                <h3>Logo</h3>
+                <img alt="Ocean Protocol logo" src={OceanLogoDark} width="90" />
+                <img alt="Ocean Protocol logo" src={OceanLogo} width="90" />
+
                 <h3>Colors</h3>
                 <Colors>
                     {Object.entries(colors).map(color => (
@@ -67,19 +89,38 @@ const Branding = () => (
                             <span className="color__value">
                                 rgb({color[1]})
                             </span>
+                            <span className="color__value">
+                                #{rgbHex(color[1])}
+                            </span>
                         </Color>
                     ))}
                 </Colors>
 
                 <h3>Gradients</h3>
-                <Colors>
+                <Gradients>
                     {Object.entries(gradients).map(gradient => (
                         <Gradient color={gradient[1]} key={gradient}>
                             <span className="color__name">${gradient[0]}</span>
                             <span className="color__value">{gradient[1]}</span>
                         </Gradient>
                     ))}
-                </Colors>
+                </Gradients>
+
+                <h3>Typography</h3>
+                <h1>Sharp Sans Display No. 1 Bold</h1>
+                <h2>Sharp Sans Display No. 1 Bold</h2>
+                <h3>Sharp Sans Display No. 1 Bold</h3>
+                <h4>Sharp Sans Display No. 1 Bold</h4>
+                <h5>Sharp Sans Display No. 1 Bold</h5>
+                <p>
+                    Sharp Sans Medium <strong>Sharp Sans Bold</strong>
+                </p>
+                <p style={{ fontSize: fonts.size.large }}>
+                    Sharp Sans Medium <strong>Sharp Sans Bold</strong>
+                </p>
+                <p style={{ fontSize: fonts.size.small }}>
+                    Sharp Sans Medium <strong>Sharp Sans Bold</strong>
+                </p>
             </ContentRow>
         </Section>
     </Fragment>
