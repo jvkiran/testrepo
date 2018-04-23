30657c006621c2b0825e82e88894cd0de14eaef3
Matthias Kretschmann <m@kretschmann.io>
2018-04-23 13:24:36 +0200

    remove redundant dynamic meta tags
    * we're not server rendered so they essentially have no effect

--- src/components/Head.js
+++ src/components/Head.js
@@ -6,20 +6,6 @@ const Head = () => (
     <Helmet
         defaultTitle={`${meta.title} — ${meta.tagline}`}
         titleTemplate={`%s — ${meta.title}`}>
-        <meta content={meta.description} name="description" />
-
-        <meta content="summary_large_image" name="twitter:card" />
-        <meta content="@oceanprotocol" name="twitter:site" />
-        <meta content={meta.title} name="twitter:title" />
-        <meta content={meta.tagline} name="twitter:description" />
-        <meta content={`${meta.url}/twitter_card.png`} name="twitter:image:src" />
-
-        <meta content={meta.title} property="og:title" />
-        <meta content="website" property="og:type" />
-        <meta content={meta.url} property="og:url" />
-        <meta content={`${meta.url}/twitter_card.png`} property="og:image" />
-        <meta content={meta.tagline} property="og:description" />
-        <meta content={meta.title} property="og:site_name" />
 
         {window.location.hostname !== 'oceanprotocol.com' &&
             <meta content="noindex,nofollow" name="robots" />
