9a98f4be44e2c24cd3fe587a1dda530a316df6fa
Matthias Kretschmann <m@kretschmann.io>
2018-04-27 11:39:06 +0200

    advisor quote concept

--- src/components/Advisors.js
+++ src/components/Advisors.js
@@ -24,13 +24,67 @@ const StyledAdvisor = styled.div`
     @media screen and (${responsive.md.min}) {
         width: calc(100% / 4 - 3rem);
     }
+`
 
-    & > img {
-        margin: 0;
+const Photo = styled.figure`
+    margin: 0;
+    border: .1rem solid rgb(${colors.pink});
+
+    &,
+    img {
         border-radius: 50%;
+        display: block;
+    }
+
+    img {
+        margin: 0;
         max-width: 100%;
         height: auto;
         filter: grayscale(100%);
+    }
+`
+
+const Quote = styled.p`
+    margin: 0;
+    font-size: ${fonts.size.small};
+    color: rgb(${colors.white});
+    position: absolute;
+    top: 50%;
+    left: -1rem;
+    right: -1rem;
+    z-index: 2;
+    background: rgb(${colors.darkGrey});
+    border: .1rem solid rgb(${colors.pink});
+    border-radius: 2px;
+    padding: .75rem 1rem;
+    box-shadow: 0 9px 18px 0 rgba(${colors.black}, .2);
+    opacity: 0;
+    transform: translate(0,-50%) scale(0);
+    transform-origin: center;
+    transition: .2s ease-out;
+
+    @media (${responsive.sm.min}) {
+        font-size: ${fonts.size.base};
+    }
+
+    @media (${responsive.md.min}) {
+        font-size: ${fonts.size.small};
+    }
+
+    &:empty {
+        display: none;
+    }
+`
+
+const QuoteWrap = styled.div`
+    position: relative;
+
+    &:hover,
+    &:focus {
+        ${Quote} {
+            opacity: 1;
+            transform: translate(0,-50%) scale(1);
+        }
     }
 `
 
@@ -77,9 +131,19 @@ const Icon = styled.a`
 
 const Advisor = ({ advisor }) => (
     <StyledAdvisor>
-        <LazyLoad once height={110} offset={100}>
-            <img alt={advisor.name} src={advisorsImg[advisor.image]} />
-        </LazyLoad>
+        <QuoteWrap>
+            <LazyLoad once height={188} offset={100}>
+                <Photo>
+                    <img alt={advisor.name} src={advisorsImg[advisor.image]} />
+                </Photo>
+            </LazyLoad>
+            <Quote>
+                {!!advisor.quote && (
+                    advisor.quote
+                )}
+            </Quote>
+        </QuoteWrap>
+
         <Name>{advisor.name}</Name>
         <Position>{advisor.position}</Position>
         <Links>
@@ -100,7 +164,6 @@ const Advisor = ({ advisor }) => (
                 </Icon>
             )}
         </Links>
-
     </StyledAdvisor>
 )
 
