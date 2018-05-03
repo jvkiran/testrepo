845853da4cf49ba0985fba959402390dca44f650
Matthias Kretschmann <m@kretschmann.io>
2018-05-03 14:11:10 +0200

    team updates

--- src/assets/team/index.js
+++ src/assets/team/index.js
@@ -3,20 +3,22 @@ import aitor from './aitor.jpg'
 import alberto from './alberto.jpg'
 import artur from './artur.jpg'
 import bruce from './bruce.jpg'
-import charlie from './charlie.jpg'
+import chalid from './chalid.jpg'
 import chirdeep from './chirdeep.jpg'
 import cristina from './cristina.jpg'
 import daniel from './daniel.jpg'
 import daryl from './daryl.jpg'
 import dimi from './dimi.jpg'
 import don from './don.jpg'
+import emily from './emily.jpg'
+import fang from './fang.jpg'
 import gautam from './gautam.jpg'
 import john from './john.jpg'
-import julian from './julian.jpg'
 import jernej from './jernej.jpg'
 import kamal from './kamal.jpg'
 import katharina from './katharina.jpg'
 import kiran from './kiran.jpg'
+import malte from './malte.jpg'
 import manuel from './manuel.jpg'
 import masha from './masha.jpg'
 import matthias from './matthias.jpg'
@@ -26,12 +28,12 @@ import oana from './oana.jpg'
 import ricardo from './ricardo.jpg'
 import sarah from './sarah.jpg'
 import shahbaz from './shahbaz.jpg'
-import sylvain from './sylvain.jpg'
+import sheridan from './sheridan.jpg'
 import tim from './tim.jpg'
 import timothy from './timothy.jpg'
 import trent from './trent.jpg'
 import troy from './troy.jpg'
-import wojciech from './wojciech.jpg'
+import vanshdeep from './vanshdeep.jpg'
 
 export default {
     ahmed,
@@ -39,20 +41,22 @@ export default {
     alberto,
     artur,
     bruce,
-    charlie,
+    chalid,
     chirdeep,
     cristina,
     daniel,
     daryl,
     dimi,
     don,
+    emily,
+    fang,
     gautam,
     jernej,
     john,
-    julian,
     kamal,
     katharina,
     kiran,
+    malte,
     manuel,
     masha,
     matthias,
@@ -62,10 +66,10 @@ export default {
     ricardo,
     sarah,
     shahbaz,
-    sylvain,
+    sheridan,
     tim,
     timothy,
     trent,
     troy,
-    wojciech
+    vanshdeep,
 }
