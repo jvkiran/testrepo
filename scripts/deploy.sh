951135737b7e51ff816a33706c4795740f507c5d
Matthias Kretschmann <m@kretschmann.io>
2018-05-25 16:51:59 +0200

    set cache-control headers during deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,19 +2,30 @@
 
 set -e;
 
+AWS_S3_BUCKET="oceanprotocol.com"
+AWS_S3_BUCKET_BETA="beta.oceanprotocol.com"
+
+function s3sync {
+    # long caching for everything, except html & pdf files
+    aws s3 sync ./build s3://"$1" --exclude "*.html" --exclude "*.pdf" --cache-control max-age=31536000,public --delete --acl public-read
+
+    # no caching for html & pdf files
+    aws s3 sync ./build s3://"$1" --exclude "*" --include "*.html" --include "*.pdf" --cache-control max-age=0,no-cache,no-store,must-revalidate --delete --acl public-read
+}
+
 ##
 ## check for pull request against master
 ##
 if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
 
-    aws s3 sync ./build s3://beta.oceanprotocol.com --delete --acl public-read
+    s3sync $AWS_S3_BUCKET_BETA
 
 ##
 ## check for master push which is no pull request
 ##
 elif [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 
-    aws s3 sync ./build s3://oceanprotocol.com --delete --acl public-read
+    s3sync $AWS_S3_BUCKET
 
     # purge full Cloudflare cache
     curl -X DELETE "https://api.cloudflare.com/client/v4/zones/$CLOUDFLARE_ZONE/purge_cache" \
