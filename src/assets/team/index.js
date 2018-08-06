9fda09304031f8334d1453edbe208103375bcd46
Matthias Kretschmann <m@kretschmann.io>
2018-08-06 11:20:39 +0200

    remove core BigchainDB people

--- src/assets/team/index.js
+++ src/assets/team/index.js
@@ -9,39 +9,27 @@ import chalid from './chalid.jpg'
 import chirdeep from './chirdeep.jpg'
 import chris from './chris.jpg'
 import cristina from './cristina.jpg'
-import daniel from './daniel.jpg'
 import daryl from './daryl.jpg'
 import dimi from './dimi.jpg'
 import don from './don.jpg'
 import emily from './emily.jpg'
 import fang from './fang.jpg'
-import gautam from './gautam.jpg'
 import irene from './irene.jpg'
 import john from './john.jpg'
-import jernej from './jernej.jpg'
-import kamal from './kamal.jpg'
-import katharina from './katharina.jpg'
 import kiran from './kiran.jpg'
 import kyriakos from './kyriakos.jpg'
 import malte from './malte.jpg'
-import manuel from './manuel.jpg'
 import masha from './masha.jpg'
 import matthias from './matthias.jpg'
 import mike from './mike.jpg'
-import muawia from './muawia.jpg'
 import oana from './oana.jpg'
 import patty from './patty.jpg'
 import paul from './paul.jpg'
-import ricardo from './ricardo.jpg'
 import sami from './sami.jpg'
 import sarah from './sarah.jpg'
-import shahbaz from './shahbaz.jpg'
 import sheridan from './sheridan.jpg'
-import tim from './tim.jpg'
 import timothy from './timothy.jpg'
 import trent from './trent.jpg'
-import troy from './troy.jpg'
-import vanshdeep from './vanshdeep.jpg'
 import yvonn from './yvonn.jpg'
 
 export default {
@@ -56,38 +44,26 @@ export default {
     chirdeep,
     chris,
     cristina,
-    daniel,
     daryl,
     dimi,
     don,
     emily,
     fang,
-    gautam,
     irene,
-    jernej,
     john,
-    kamal,
-    katharina,
     kiran,
     kyriakos,
     malte,
-    manuel,
     masha,
     matthias,
     mike,
-    muawia,
     oana,
     patty,
     paul,
-    ricardo,
     sami,
     sarah,
-    shahbaz,
     sheridan,
-    tim,
     timothy,
     trent,
-    troy,
-    vanshdeep,
     yvonn
 }
