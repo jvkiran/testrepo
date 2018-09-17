86605e0e208cfeeac819ddfc955a8b1b5f84afd8
Matthias Kretschmann <m@kretschmann.io>
2018-09-17 14:51:30 +0200

    performance tweaks

--- src/sections/Blog.js
+++ src/sections/Blog.js
@@ -1,6 +1,7 @@
-import React, { PureComponent } from 'react'
+import React, { PureComponent, Fragment } from 'react'
 import LazyLoad from 'react-lazyload'
 import axios from 'axios'
+import Helmet from 'react-helmet'
 import FadeIn from '../components/FadeIn'
 import Title from '../components/Title'
 import Section from '../components/Section'
@@ -17,6 +18,8 @@ import {
     StyledSubtitle,
     StyledAction
 } from './Blog.css'
+
+const url = 'https://wt-bfc3ae9804422f8a4ea114dc7c403296-0.run.webtask.io/medium/oceanprotocol'
 
 export default class Blog extends PureComponent {
     signal = axios.CancelToken.source()
@@ -37,7 +40,7 @@ export default class Blog extends PureComponent {
     fetchPosts = async () => {
         try {
             this.setState({ fetching: true })
-            const response = await axios.get('https://wt-bfc3ae9804422f8a4ea114dc7c403296-0.run.webtask.io/medium/oceanprotocol', {
+            const response = await axios.get(url, {
                 cancelToken: this.signal.token
             })
             const lastPosts = response.data.slice(0, 3)
@@ -55,39 +58,44 @@ export default class Blog extends PureComponent {
         const { posts, fetching } = this.state
 
         return (
-            <Section id="blog" minHeight={930}>
-                <ContentRow>
-                    <Title>Learn more about Ocean Protocol</Title>
-                    {fetching ? (
-                        <Spinner />
-                    ) : (
-                        <LazyLoad once height={524} offset={100}>
-                            <FadeIn>
-                                <Grid>
-                                    {posts.map(post => (
-                                        <Cell smallGutter key={post.id} width={1 / 3}>
-                                            <a href={post.postUrl}>
-                                                <StyledCard>
-                                                    <StyledHeader imageUrl={post.imageUrl} />
-                                                    <StyledContent>
-                                                        <StyledTitle>{post.title}</StyledTitle>
-                                                        <StyledSubtitle>{post.subtitle}</StyledSubtitle>
-                                                    </StyledContent>
-                                                </StyledCard>
-                                            </a>
-                                        </Cell>
-                                    ))}
-                                </Grid>
-                            </FadeIn>
-                        </LazyLoad>
-                    )}
-                    <StyledAction fetching={fetching}>
-                        <a href={social.blog}>
-                            Go to Blog
-                        </a>
-                    </StyledAction>
-                </ContentRow>
-            </Section>
+            <Fragment>
+                <Helmet>
+                    <link rel="preconnect" href={url} />
+                </Helmet>
+                <Section id="blog" minHeight={930}>
+                    <ContentRow>
+                        <Title>Learn more about Ocean Protocol</Title>
+                        {fetching ? (
+                            <Spinner />
+                        ) : (
+                            <LazyLoad once height={524} offset={100}>
+                                <FadeIn>
+                                    <Grid>
+                                        {posts.map(post => (
+                                            <Cell smallGutter key={post.id} width={1 / 3}>
+                                                <a href={post.postUrl}>
+                                                    <StyledCard>
+                                                        <StyledHeader imageUrl={post.imageUrl} />
+                                                        <StyledContent>
+                                                            <StyledTitle>{post.title}</StyledTitle>
+                                                            <StyledSubtitle>{post.subtitle}</StyledSubtitle>
+                                                        </StyledContent>
+                                                    </StyledCard>
+                                                </a>
+                                            </Cell>
+                                        ))}
+                                    </Grid>
+                                </FadeIn>
+                            </LazyLoad>
+                        )}
+                        <StyledAction fetching={fetching}>
+                            <a href={social.blog}>
+                                Go to Blog
+                            </a>
+                        </StyledAction>
+                    </ContentRow>
+                </Section>
+            </Fragment>
         )
     }
 }
