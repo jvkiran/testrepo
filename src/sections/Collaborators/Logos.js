cd85846e665a1f28189b942f6af93a1f304dcab9
Matthias Kretschmann <m@kretschmann.io>
2018-09-20 15:39:55 +0200

    more logos

--- src/sections/Collaborators/Logos.js
+++ src/sections/Collaborators/Logos.js
@@ -4,23 +4,33 @@
 //
 
 import Agorai from '../../components/svg/logos/Agorai'
+import Anaconda from '../../components/svg/logos/Anaconda'
 import Aragon from '../../components/svg/logos/Aragon'
 import Bigchaindb from '../../components/svg/logos/Bigchaindb'
 import Dex from '../../components/svg/logos/Dex'
+import Digitalcatapult from '../../components/svg/logos/Digitalcatapult'
+import Enigma from '../../components/svg/logos/Enigma'
+import Fitchain from '../../components/svg/logos/Fitchain'
 import Innogy from '../../components/svg/logos/Innogy'
 import Mattereum from '../../components/svg/logos/Mattereum'
 import Truebit from '../../components/svg/logos/Truebit'
 import Weeve from '../../components/svg/logos/Weeve'
 import Web3foundation from '../../components/svg/logos/Web3foundation'
+import Xprize from '../../components/svg/logos/Xprize'
 
 export default {
     Agorai,
+    Anaconda,
     Aragon,
     Bigchaindb,
     Dex,
+    Digitalcatapult,
+    Enigma,
+    Fitchain,
     Innogy,
     Mattereum,
     Truebit,
     Weeve,
-    Web3foundation
+    Web3foundation,
+    Xprize
 }
