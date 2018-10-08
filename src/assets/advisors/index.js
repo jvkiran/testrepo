edb056044c6fbaf9a31052cd68b9c95a57f9ce57
Matthias Kretschmann <m@kretschmann.io>
2018-10-08 16:43:07 +0200

    new advisor

--- src/assets/advisors/index.js
+++ src/assets/advisors/index.js
@@ -1,7 +1,9 @@
 import carsten from './carsten.jpg'
 import meltem from './meltem.jpg'
+import sebastian from './sebastian.jpg'
 
 export default {
     carsten,
-    meltem
+    meltem,
+    sebastian
 }
