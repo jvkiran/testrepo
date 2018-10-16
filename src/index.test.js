d271014d52f72dad89d2cabc4447db3686e9513e
Matthias Kretschmann <m@kretschmann.io>
2018-10-16 19:19:37 +0200

    update to styled-components 4

--- src/index.test.js
+++ src/index.test.js
@@ -1,12 +1,7 @@
 import './lib/polyfills'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { injectGlobal } from 'styled-components'
-
-import { globalStyles } from './styles'
 import App from './App'
-
-injectGlobal`${globalStyles}`
 
 it('renders without crashing', () => {
     const div = document.createElement('div')
