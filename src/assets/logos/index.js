355b090716be25fbfacd619b4b633d0b93046b78
Matthias Kretschmann <m@kretschmann.io>
2018-11-21 16:02:20 +0100

    hide some collaborators again

--- src/assets/logos/index.js
+++ src/assets/logos/index.js
@@ -5,21 +5,21 @@
 import { ReactComponent as Couger } from '../../assets/logos/couger.svg'
 import { ReactComponent as Enigma } from '../../assets/logos/enigma.svg'
 import { ReactComponent as Fitchain } from '../../assets/logos/fitchain.svg'
-import { ReactComponent as Imda } from '../../assets/logos/imda.svg'
+// import { ReactComponent as Imda } from '../../assets/logos/imda.svg'
 import { ReactComponent as Messari } from '../../assets/logos/messari.svg'
 import { ReactComponent as Mobi } from '../../assets/logos/mobi.svg'
-import { ReactComponent as Roche } from '../../assets/logos/roche.svg'
-import { ReactComponent as Unilever } from '../../assets/logos/unilever.svg'
+// import { ReactComponent as Roche } from '../../assets/logos/roche.svg'
+// import { ReactComponent as Unilever } from '../../assets/logos/unilever.svg'
 import { ReactComponent as Xprize } from '../../assets/logos/xprize.svg'
 
 export default {
     Couger,
     Enigma,
     Fitchain,
-    Imda,
+    // Imda,
     Messari,
     Mobi,
-    Roche,
-    Unilever,
+    // Roche,
+    // Unilever,
     Xprize
 }
