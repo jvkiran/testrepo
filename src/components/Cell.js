04bc153c660d3ad1c9c46c131b33e3a50ad2daf5
Matthias Kretschmann <m@kretschmann.io>
2019-01-07 09:37:11 +0100

    package updates

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { Container } from './Cell.css'
 
 const Cell = ({ children, ...props }) => (
     <Container {...props}>{children}</Container>
 )
+
+Cell.propTypes = {
+    children: PropTypes.any
+}
 
 export default Cell
