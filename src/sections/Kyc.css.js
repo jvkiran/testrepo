f6fc672a4fea727537aaa316602796dab369ba21
Matthias Kretschmann <m@kretschmann.io>
2019-02-01 16:05:07 +0100

    minimal kyc section

--- src/sections/Kyc.css.js
+++ src/sections/Kyc.css.js
@@ -1,102 +1,34 @@
 import styled from 'styled-components'
-import Button from '../components/Button'
-import Grid from '../components/Grid'
 import { Line } from '../components/SectionHeader.css'
-import { colors, responsive, layout } from '../styles'
+import { layout, fonts, colors } from '../styles'
+import Button from '../components/Button'
 
-export const ContributionPlatforms = styled.div`
-    margin-bottom: calc(${layout.spacer} * 2);
-
-    @media (${responsive.md.min}) {
-        display: flex;
-        justify-content: space-between;
-    }
+export const StyledButton = styled(Button)`
+    margin: auto;
+    min-width: 20rem;
+    text-align: center;
+    padding: calc(${layout.spacer} / 1.5) ${layout.spacer};
+    font-size: ${fonts.size.base};
 `
 
-export const PlatformButton = styled(Button)`
-    width: auto;
-    position: absolute;
-    left: ${layout.spacer};
-    bottom: ${layout.spacer};
-    transition: 0.2s ease-out;
-
-    &:hover,
-    &:focus {
-        transform: none;
-    }
-`
-
-export const PlatformIntro = styled.h5`
-    width: 100%;
-    color: rgb(${colors.black});
-    margin-top: 0;
-`
-
-export const ContributionPlatform = styled.a`
-    display: flex;
-    flex-wrap: wrap;
-    align-content: flex-start;
-    padding: ${layout.spacer};
-    padding-bottom: 6rem;
-    border-radius: 0.2rem;
-    background: rgba(${colors.grey}, 0.6);
-    margin-bottom: ${layout.spacer};
+export const Fractal = styled.p`
     color: rgb(${colors.dimmedGrey});
-    position: relative;
+    font-size: ${fonts.size.base};
+    text-align: center;
 
-    p {
-        width: 100%;
-    }
-
-    ul {
-        margin-bottom: ${layout.spacer};
-    }
-
-    @media (${responsive.md.min}) {
-        flex: 0 1 48%;
-        margin-bottom: 0;
-    }
-
-    svg {
-        fill: rgb(${colors.lightGrey});
-        max-width: 12rem;
-        width: auto;
-        height: 2rem;
-        display: block;
-        margin-bottom: ${layout.spacer};
-    }
-
-    &:first-child {
-        background: rgb(${colors.white});
-        color: rgb(${colors.darkGrey});
+    a {
+        font-family: ${fonts.family.button};
+        text-transform: uppercase;
+        font-size: ${fonts.size.small};
+        display: inline-block;
+        margin-left: 0.5rem;
 
         svg {
-            fill: rgb(${colors.lightGrey});
-        }
-    }
-
-    &:last-child {
-        ${PlatformButton} {
-            background: rgb(${colors.white});
-            color: rgb(${colors.black});
-        }
-
-        ${PlatformIntro} {
-            color: rgb(${colors.dimmedGrey});
-        }
-    }
-
-    &:hover,
-    &:focus {
-        ${PlatformButton} {
-            background: rgb(${colors.black});
-            color: rgb(${colors.white});
+            fill: currentColor;
         }
     }
 `
 
-export const TokenInfo = styled(Grid)``
-
 export const Divider = styled(Line)`
-    margin-bottom: calc(${layout.spacer} * 2);
+    margin-bottom: calc(${layout.spacer} * 1.5);
 `
--- src/sections/Kyc.js
+++ src/sections/Kyc.js
@@ -1,23 +1,10 @@
 import React from 'react'
 import Section from '../components/Section'
-
-import Cell from '../components/Cell'
 import SectionHeader from '../components/SectionHeader'
 import ContentRow from '../components/ContentRow'
-import SubTitle from '../components/SubTitle'
-import { Ul, Li } from '../components/Lists'
-import { ReactComponent as CoinList } from '../assets/logos/coinlist.svg'
-import { ReactComponent as Fractal } from '../assets/logos/fractal.svg'
 import { colors } from '../styles'
-import {
-    ContributionPlatforms,
-    ContributionPlatform,
-    PlatformIntro,
-    PlatformButton,
-    TokenInfo,
-    Divider
-} from './Kyc.css'
-import Paragraph from '../components/Paragraph'
+import { ReactComponent as Arrow } from '../assets/misc/arrow.svg'
+import { StyledButton, Fractal, Divider } from './Kyc.css'
 import content from '../data/kyc.json'
 
 const Kyc = () => (
@@ -30,65 +17,21 @@ const Kyc = () => (
             title={content.title}
             description={content.description}
             white
-            line
         />
 
         <ContentRow>
-            <SubTitle left white>
-                Register
-            </SubTitle>
-
-            <ContributionPlatforms>
-                {content.platforms.map((platform, index) => (
-                    <ContributionPlatform key={index} href={platform.link}>
-                        {platform.name === 'CoinList' ? (
-                            <CoinList />
-                        ) : (
-                            <Fractal />
-                        )}
-
-                        <PlatformIntro
-                            dangerouslySetInnerHTML={{ __html: platform.intro }}
-                        />
-
-                        {platform.list && (
-                            <Ul>
-                                {platform.list.map((item, index) => (
-                                    <Li key={index}>{item}</Li>
-                                ))}
-                            </Ul>
-                        )}
-
-                        <PlatformButton primary>
-                            Go to {platform.name}
-                        </PlatformButton>
-                    </ContributionPlatform>
-                ))}
-            </ContributionPlatforms>
+            <StyledButton primary large href={content.button.link}>
+                {content.button.title}
+            </StyledButton>
 
             <Divider />
 
-            <TokenInfo>
-                <Cell width={1 / 3}>
-                    <SubTitle white left>
-                        {content.tokeninfo.title}
-                    </SubTitle>
-                </Cell>
-                <Cell width={2 / 3}>
-                    <Paragraph white>{content.tokeninfo.description}</Paragraph>
-
-                    <Ul>
-                        {content.tokeninfo.metrics.map((metric, index) => (
-                            <Li
-                                key={index}
-                                dangerouslySetInnerHTML={{
-                                    __html: metric
-                                }}
-                            />
-                        ))}
-                    </Ul>
-                </Cell>
-            </TokenInfo>
+            <Fractal>
+                {content.fractal.title}
+                <a href={content.fractal.button.link}>
+                    {content.fractal.button.title} <Arrow />
+                </a>
+            </Fractal>
         </ContentRow>
     </Section>
 )
