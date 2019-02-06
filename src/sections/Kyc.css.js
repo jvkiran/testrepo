a593dacb38f5cb3ac9eb558e749efd0ac3fa0760
Matthias Kretschmann <m@kretschmann.io>
2019-02-06 12:13:33 +0100

    remove Fractal

--- src/sections/Kyc.css.js
+++ src/sections/Kyc.css.js
@@ -1,6 +1,5 @@
 import styled from 'styled-components'
-import { Line } from '../components/SectionHeader.css'
-import { layout, fonts, colors } from '../styles'
+import { layout, fonts } from '../styles'
 import Button from '../components/Button'
 
 export const StyledButton = styled(Button)`
@@ -9,27 +8,4 @@ export const StyledButton = styled(Button)`
     text-align: center;
     padding: calc(${layout.spacer} / 1.5) ${layout.spacer};
     font-size: ${fonts.size.base};
-`
-
-export const Fractal = styled.p`
-    color: rgb(${colors.dimmedGrey});
-    font-size: ${fonts.size.base};
-    text-align: center;
-
-    a {
-        font-family: ${fonts.family.button};
-        text-transform: uppercase;
-        font-size: ${fonts.size.small};
-        display: inline-block;
-        margin-left: 0.5rem;
-
-        svg {
-            fill: currentColor;
-        }
-    }
-`
-
-export const Divider = styled(Line)`
-    margin-top: calc(${layout.spacer} * 1.5);
-    margin-bottom: calc(${layout.spacer} * 1.5);
 `
